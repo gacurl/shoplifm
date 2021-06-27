3faa083e0cc48df7c508872019157ddfd3cacc3a
Greg Curl <greg.a.curl@gmail.com>
2021-06-27 16:37:13 -0400

    adding checkout to current_user.stripe_customer_id

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -4,6 +4,7 @@ class CheckoutController < ApplicationController
     product = Product.find(params[:id])
 
     @session = Stripe::Checkout::Session.create({
+      customer: current_user.stripe_customer_id,
       payment_method_types: ['card'],
       line_items: [{
         name: product.name,
